fb3599f77cc0cdcb2ba092da76cf1d85cc224c87
ud3v0id <ppz@tuta.io>
2024-10-22 16:04:06 +0800

    update part1 script

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,20 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
+echo >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon;master' >> feeds.conf.default
+echo 'src-git argon_config https://github.com/jerrykuku/luci-app-argon-config;master' >> feeds.conf.default
+
+# Linux DVB driver for NERC DTV (CY68013A+HDIC HD2312) DTMB/DMB-TH USB adapter
+HD2312="$(pwd)/hd2312"
+git clone --depth 1 --branch master https://github.com/hanwckf/hd2312.git $HD2312
+
+cat $HD2312/openwrt/dvb-kconfig >> target/linux/generic/config-[0-9].[0-9]*
+cp $HD2312/openwrt/dvb.mk package/kernel/linux/modules/
+
+mkdir package/hd2312
+cp $HD2312/openwrt/Makefile package/hd2312/
+
